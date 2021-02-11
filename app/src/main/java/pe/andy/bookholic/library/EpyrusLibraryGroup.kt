a1886c06d416ce29b33f7de81a96a3baba625c22
Andy <strawberryit@daum.net>
2021-02-11 19:34:57 +0900

    양천구 e도서관 제외

--- app/src/main/java/pe/andy/bookholic/library/EpyrusLibraryGroup.kt
+++ app/src/main/java/pe/andy/bookholic/library/EpyrusLibraryGroup.kt
@@ -2,15 +2,17 @@ package pe.andy.bookholic.library
 
 import pe.andy.bookholic.MainActivity
 import pe.andy.bookholic.model.Library
-import pe.andy.bookholic.model.Library.Companion.Encoding_EUCKR
 import pe.andy.bookholic.searcher.EpyrusLibrarySearchTask
 import pe.andy.bookholic.searcher.LibrarySearchTask
 import java.lang.ref.SoftReference
 
 object EpyrusLibraryGroup {
+    /*
     private val libraries = listOf(
             Library(name = "양천구 e도서관", url = "http://ebook2.yangcheon.go.kr", encoding = Encoding_EUCKR)
     )
+    */
+    private val libraries = emptyList<Library>()
 
     fun getLibraryList(mainActivity: MainActivity): List<LibrarySearchTask> {
         return libraries.map {
