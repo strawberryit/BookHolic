0201d719c02f46c326dca9a6ff80e587cf856256
Andy <strawberryit@daum.net>
2021-05-16 14:58:22 +0900

    진주시립도서관 검색 불가로 제외

--- app/src/main/java/pe/andy/bookholic/library/FxLibraryGroup.kt
+++ app/src/main/java/pe/andy/bookholic/library/FxLibraryGroup.kt
@@ -7,9 +7,7 @@ import pe.andy.bookholic.searcher.LibrarySearchTask
 import java.lang.ref.SoftReference
 
 object FxLibraryGroup {
-    private val libraries = listOf(
-            Library(name = "진주시립도서관", url = "http://125.135.250.131:8080")
-    )
+    private val libraries = emptyList<Library>()
 
     fun getLibraryList(mainActivity: MainActivity): List<LibrarySearchTask> {
         return libraries.map {
