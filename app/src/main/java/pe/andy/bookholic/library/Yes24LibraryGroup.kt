a384fbb7852e4b404fe68baa671646058567f8c1
Andy <strawberryit@daum.net>
2022-03-13 21:27:09 +0900

    fix: 여의도 전자도서관 도메인 변경 대응

--- app/src/main/java/pe/andy/bookholic/library/Yes24LibraryGroup.kt
+++ app/src/main/java/pe/andy/bookholic/library/Yes24LibraryGroup.kt
@@ -15,7 +15,7 @@ object Yes24LibraryGroup {
             Yes24Library(name = "전라남도 도립도서관", url = "http://152.99.134.221:8080", encoding = Encoding_EUCKR, yes24Type = TypeB),
             Yes24Library(name = "울주통합도서관", url = "http://uljuebook.ulju.ulsan.kr", encoding = Encoding_EUCKR, yes24Type = TypeB),
             Yes24Library(name = "영천시립도서관", url = "http://www.yclib.go.kr:8080", encoding = Encoding_EUCKR, yes24Type = TypeA),
-            Yes24Library(name = "여의도 전자책도서관", url = "http://ebook.yulib.or.kr", encoding = Encoding_EUCKR, yes24Type = TypeA),
+            Yes24Library(name = "여의도 전자책도서관", url = "https://ebook.ydplib.or.kr", encoding = Encoding_EUCKR, yes24Type = TypeA),
             Yes24Library(name = "경산시립도서관", url = "http://elib.gbgs.go.kr", encoding = Encoding_EUCKR, yes24Type = TypeA),
             Yes24Library(name = "강서구 전자도서관", url = "https://ebook.gangseo.seoul.kr", encoding = Encoding_EUCKR, yes24Type = TypeB),
             Yes24Library(name = "광주남구 통합도서관", url = "http://ebook.namgu.gwangju.kr:8080", encoding = Encoding_EUCKR, yes24Type = TypeA),
