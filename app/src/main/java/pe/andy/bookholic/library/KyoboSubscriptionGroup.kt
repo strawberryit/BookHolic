8c0d17859c2c94f1fe9141ebe1d9d0265751c5ab
Andy <strawberryit@daum.net>
2023-05-26 23:50:06 +0900

    fix: 접속오류 도서관 URL 변경

--- app/src/main/java/pe/andy/bookholic/library/KyoboSubscriptionGroup.kt
+++ app/src/main/java/pe/andy/bookholic/library/KyoboSubscriptionGroup.kt
@@ -8,12 +8,14 @@ import java.lang.ref.SoftReference
 
 object KyoboSubscriptionGroup {
     private val libraries = listOf(
-            Library(name = "영등포 구립도서관 (구독)", url = "http://ydplib.dkyobobook.co.kr"),
-            Library(name = "용산구립도서관", url = "http://ebook.yslibrary.or.kr/elibrary-front"),
-            Library(name = "서초구 전자도서관", url = "http://ebook.seocholib.or.kr", path="/elibrary-front"),
+            Library(name = "영등포 구립도서관 (구독)", url = "https://ydplib.dkyobobook.co.kr"),
+            Library(name = "용산구립도서관", url = "https://ebook.yslibrary.or.kr/elibrary-front"),
+            Library(name = "서초구 전자도서관", url = "https://ebook.seocholib.or.kr", path="/elibrary-front"),
             Library(name = "중구 통합전자도서관", url = "https://ebook.junggulib.or.kr/elibrary-front"),
+            Library(name = "광양시립도서관 (소장)", url = "https://ebook.gwangyang.go.kr:444", path="/elibrary-front"),
             Library(name = "광양시립도서관 (구독)", url = "https://gwangyanglib.dkyobobook.co.kr"),
             Library(name = "경기도사이버도서관 (구독)", url = "https://cyberlibrary.dkyobobook.co.kr"),
+            Library(name = "김제시립도서관", url = "https://ebook-gjl.gimje.go.kr:444/elibrary-front/"),
         )
 
     fun getLibraryList(searchFragment: SearchFragment): List<LibrarySearchTask> {
--- app/src/main/java/pe/andy/bookholic/library/Yes24LibraryGroup.kt
+++ app/src/main/java/pe/andy/bookholic/library/Yes24LibraryGroup.kt
@@ -11,12 +11,11 @@ import java.lang.ref.SoftReference
 
 object Yes24LibraryGroup {
     private val libraries = listOf(
-            Yes24Library(name = "경주시립도서관", url = "http://library.gyeongju.go.kr:81", encoding = Encoding_EUCKR, yes24Type = TypeA),
             Yes24Library(name = "전라남도 도립도서관", url = "http://152.99.134.221:8080", encoding = Encoding_EUCKR, yes24Type = TypeB),
             Yes24Library(name = "울주통합도서관", url = "http://uljuebook.ulju.ulsan.kr", encoding = Encoding_EUCKR, yes24Type = TypeB),
             Yes24Library(name = "영천시립도서관", url = "http://www.yclib.go.kr:8080", encoding = Encoding_EUCKR, yes24Type = TypeA),
             Yes24Library(name = "여의도 전자책도서관", url = "https://ebook.ydplib.or.kr", encoding = Encoding_EUCKR, yes24Type = TypeA),
-            Yes24Library(name = "경산시립도서관", url = "http://elib.gbgs.go.kr", encoding = Encoding_EUCKR, yes24Type = TypeA),
+            Yes24Library(name = "경산시립도서관", url = "https://elib.gbgs.go.kr", encoding = Encoding_EUCKR, yes24Type = TypeA),
             Yes24Library(name = "강서구 전자도서관", url = "https://ebook.gangseo.seoul.kr", encoding = Encoding_EUCKR, yes24Type = TypeB),
             Yes24Library(name = "광주남구 통합도서관", url = "http://ebook.namgu.gwangju.kr:8080", encoding = Encoding_EUCKR, yes24Type = TypeA),
     )
